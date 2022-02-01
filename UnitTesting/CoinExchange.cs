0080f7ab9114a9a2188ba5b859089612c1317aa1
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-01 08:11:01 +0530

    [Ashish] Refractor:Coin Exchange Problem is Modified Removed Lot of ifelse

--- UnitTesting/CoinExchange.cs
+++ UnitTesting/CoinExchange.cs
@@ -6,83 +6,36 @@ using System.Threading.Tasks;
 
 namespace UnitTesting
 {
-     public  class CoinExchange
+    public class CoinExchange
     {
-        Dictionary<int, int> Currencies = new Dictionary<int, int>()
+        public void Notes(int amount)
         {
-            {1000,0 },
-            {500,0},
-            {100,0 },
-            {50,0 },
-            {20,0 },
-            {10,0 },
-            {5,0 },
-            {2,0 },
-            {1,0 },
-        };
-        public int Notes(int amount)
-        {
-            if (amount == 0) return 0;
+           // Console.WriteLine(amount + " ");
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] frequency = new int[notes.Length];
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount > notes[i])
+                {
+                    frequency[i] = amount / notes[i];
+                    amount -= notes[i] * frequency[i];
+                }
+            }
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if(frequency[i]!=0)
+                    Console.WriteLine("The Frequency of {0} is {1}",notes[i],frequency[i]);
+            }
 
-            if (amount > 1000)
-            {
-                
-                Currencies[1000]=amount/1000;
-                return (amount/1000)+Notes(amount % 1000);
-            }
-            if (amount > 500)
-            {
-                Currencies[500]=amount/500;
-                return (amount/500)+Notes(amount % 500);
-            }
-            if (amount > 100)
-            {
-                Currencies[100]=amount/100;
-                return (amount/100)+Notes(amount%100);
-            }
-            if (amount > 50)
-            {
-                Currencies[50]=amount/50;
-                return amount/50+Notes(amount % 50);
-            }
-            if (amount > 10)
-            {
-                Currencies[10]=amount/10;
-                return (amount/10)+Notes(amount %10);
-            }
-            if (amount > 5)
-            {
-                Currencies[5] = amount / 5;
-                return (amount/5)+Notes(amount %5);
-            }
-            if (amount > 2)
-            {
-                Currencies[2]=amount/2;
-                return (amount/2)+Notes(amount % 2);
-            }
-            else
-            {
-                
-                    Currencies[1]++;
-                    return 1+Notes(amount - 1);
-                
-            }
 
         }
         public void Change()
         {
             Console.WriteLine("Enter Your Amount\n");
-            int amount =Convert.ToInt32(Console.ReadLine());
-            int number = Notes(amount);
-            Console.WriteLine("The Minimum Notes required are {0}", number);
-            Console.WriteLine("The Respective Domination required will be\n");
-            Console.WriteLine("Value        NumberOfNotes\n");
-            foreach (KeyValuePair<int, int> i in Currencies)
-            {
-                if(i.Value>0)
-                Console.WriteLine(i.Key+"       "+ i.Value);
-            }
-    
+            int amount = Convert.ToInt32(Console.ReadLine());
+             Notes(amount);
+            
+
         }
     }
 }
